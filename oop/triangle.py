dff8b3df5ba73ff66cc8a13bc073da16cd1b19a7
Yossi Ben Haim <v0585204770@gmail.com>
2025-06-24 17:35:37 +0300

    added the from rectangle import Rectangle

---
+++ oop/triangle.py
@@ -0,0 +1,12 @@
+from rectangle import Rectangle
+
+class Triangle(Rectangle):
+
+    def get_area(self):
+        return (self.length * self.width) / 2
+
+    def __str__(self):
+        return f"the length of triangle is {self.length}, the width of triangle is {self.width}"
+
+    def __repr__(self):
+        return f"triangle ({self.width},{self.length})"
