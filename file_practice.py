ef11785fc4582e976b094af5f59aa52dde7c2070
Yossi Ben Haim <v0585204770@gmail.com>
2025-06-24 11:45:13 +0300

    added the q_4

--- file_practice.py
+++ file_practice.py
@@ -22,6 +22,42 @@ def q_3(name_file):
         if check:
             print(line)
 
+def q_4(name_file):
+    with open(name_file,"r") as file:
+        data = file.readlines()
+    count_line_odd = 0
+    count_word = 0
+    count_char = 0
+    dict_char = {}
+    for line in data:
+        line = line.split()
+        if not len(line) % 2:
+            count_line_odd += 1
+        for word in line:
+            try:
+                int(word)
+            except ValueError:
+                count_word += 1
+                if word:
+                    count_char += len(word)
+                    for char in word:
+                        if char in dict_char:
+                            dict_char[char] += 1
+                        else:
+                            dict_char[char] = 1
+    big_key = ""
+    big_value = 0
+    for key,value in dict_char.items():
+        if value > big_value:
+            big_key = key
+            big_value = value
+    print(dict_char)
+    print(f"the count of lines is: {count_line_odd}")
+    print(f"the count of words is: {count_word}")
+    print(f"the count of char is: {count_char}")
+    print(f"the char of show Most is: {big_key}, is show {big_value}")
+
 name_file = "my_text.txt"
-q_2(name_file)
-q_3(name_file)
+# q_2(name_file)
+# q_3(name_file)
+q_4(name_file)
