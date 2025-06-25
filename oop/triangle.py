16b530e01e752a9e07a27d39d8e878e7a76c67e2
Yossi Ben Haim <v0585204770@gmail.com>
2025-06-25 09:02:07 +0300

    fixed the triangle

--- oop/triangle.py
+++ oop/triangle.py
@@ -6,7 +6,7 @@ class Triangle(Rectangle):
         return (self.length * self.width) / 2
 
     def get_perimeter(self):
-        return (self.width **2 + self.length **)
+        return (self.width **2 + self.length **2) ** -2
 
     def __str__(self):
         return f"the length of triangle is {self.length}, the width of triangle is {self.width}"
